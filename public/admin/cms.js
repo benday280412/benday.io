1eaa629c41907f47bec47da4393781706d41cb0b
Ben Day <ben.day@johnlewis.co.uk>
2021-10-20 17:01:34 +0100

    New gitignore

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9c525483ce36a2de1642";
+/******/ 	var hotCurrentHash = "5c465ba4ac5079fa74d2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -80743,7 +80743,7 @@ var _default = Testimonials;
 
 // extracted by mini-css-extract-plugin
     if(true) {
-      // 1634742474868
+      // 1634745518004
       var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
